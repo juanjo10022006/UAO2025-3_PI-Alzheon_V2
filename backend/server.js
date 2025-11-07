191d140c0547785d09b2f54963d7802e926b4464
alejandrosg124 <alejo.solarte1024@gmail.com>
2025-11-07 13:16:04 -0500

    port switch

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use('/api', router);
 connectDB();
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT,  () => {
     console.log(`Server is running on port ${PORT}`);
 })
