794dcd887fe623ea5f73686e244bdd6f5e4c8feb
alejandrosg124 <alejo.solarte1024@gmail.com>
2025-11-07 13:05:28 -0500

    cambio puerto api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 
 
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001'
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5500'
 
 export interface UserResponse {
   _id: string
