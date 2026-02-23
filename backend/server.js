233adb0585679c7f9e038eaaa9863c4eb31523f9
Jorge Andrés Medina <jorgeamedinau@gmail.com>
2026-02-23 18:26:03 -0500

    Add additional allowed origins for CORS

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+    'http://localhost:3001',
+    'http://127.0.0.1:3001',
 ];
 
 // Agregar orígenes adicionales desde .env si existen
